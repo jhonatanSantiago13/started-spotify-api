b5c10f2e6599c4cc96d70d20d74ec1cb11e7a126
jhonatanSantiago13 <jhonale44@hotmail.com>
2022-08-02 16:52:11 -0500

    buscador de id

--- sp.js
+++ sp.js
@@ -46,7 +46,7 @@ const _getToken = async () =>{
 		});
 
 		const data = await result.json();
-	return data.access_token;
+	    return data.access_token;
 
 	} catch(error){
 
@@ -93,5 +93,106 @@ const test = async () => {
 
 }
 
+// test();
 
-test();
+const getAlbum = async (artist) => {
+
+	const token = await _getToken();
+
+	// https://api.spotify.com/v1/search/?q=${artist}&type=artist
+
+	fetch(`https://api.spotify.com/v1/artists/${artist}/albums`,{
+
+		method: "GET",
+		headers: {
+	    Authorization: `Bearer ${token}`
+	  	}
+	})
+
+	.then(response => response.json())
+	.then(response => {
+
+		// console.log("response", response.items);
+
+		const albums = response.items;
+
+		albums.forEach((data, index)=>{
+
+			console.log("album", data.name);
+
+		})
+
+	})
+
+}
+
+// getAlbum("06HL4z0CvFAxyc27GXpf02");
+
+const getIdArtist = async (name) => {
+
+	const token = await _getToken();
+
+	fetch(`https://api.spotify.com/v1/search?q=${name}&type=artist`, {
+
+		method: "GET",
+		headers: {
+	    Authorization: `Bearer ${token}`
+	  	}
+
+	})
+	.then(response => response.json())
+	.then(response =>{
+  	
+  		// console.log("response", response.artists.items);
+
+  		const artist =  response.artists.items;
+
+  		console.log("id", response.artists.items[0].id);
+
+  		/*artist.forEach((data, index) =>{
+
+  			console.log("id", data.id);
+
+  		})*/
+
+	})
+
+}
+
+// getIdArtist("taylor%20swift");
+
+
+const getIdArtist1 = async (name) => {
+
+	try {
+
+		const token = await _getToken();
+
+		const response = await fetch(`https://api.spotify.com/v1/search?q=${name}&type=artist`, {
+
+			method: "GET",
+			headers: {
+		    Authorization: `Bearer ${token}`
+		  	}
+
+		})
+		
+		const data = await response.json();
+
+		// console.log("data", data.artists.items[0].id);
+
+		return data.artists.items[0].id;
+
+
+
+		
+	} catch(error) {
+		
+		console.log(error);
+
+	}
+
+	
+}
+
+getIdArtist1("taylor%20swift").then((value)=>console.log("id", value));
